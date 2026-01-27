b2c126b4fed755d23e04dd1ade3477ebd25d618c
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-01-27 21:00:51 +0700

    1

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -11,12 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production and ?
+# Quick-start development settings -- unsuitable for production and ?
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
