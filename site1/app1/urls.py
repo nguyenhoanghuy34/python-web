da1341a8316f160a8abfbc43fa886ac053f4d52c
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-01-28 21:05:40 +0700

    app1/1

--- site1/app1/urls.py
+++ site1/app1/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('/1', views.index1)
 ]
--- site1/app1/views.py
+++ site1/app1/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('This is app1')
+def index1(request):
+    return HttpResponse('This is child 1')
